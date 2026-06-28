1cb80851533c8959be51763eb8e50440605059e1
Geanderson <geanderson@nexustreinamento.com>
2026-06-28 11:11:03 -0300

    hotfix: revert manual CookieStorage to default Amplify SSR behavior to fix production login hanging

--- src/components/providers/amplify-provider.tsx
+++ src/components/providers/amplify-provider.tsx
@@ -1,22 +1,10 @@
 "use client";
 
 import { Amplify } from "aws-amplify";
-import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
-import { CookieStorage } from "aws-amplify/utils";
 import React, { ReactNode } from "react";
 import outputs from "@/../amplify_outputs.json";
 
 Amplify.configure(outputs, { ssr: true });
-
-if (typeof window !== "undefined") {
-  cognitoUserPoolsTokenProvider.setKeyValueStorage(
-    new CookieStorage({
-      secure: window.location.protocol === "https:",
-      path: "/",
-      sameSite: "lax",
-    })
-  );
-}
 
 export function AmplifyProvider({ children }: { children: ReactNode }) {
   return <>{children}</>;
