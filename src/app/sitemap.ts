78eab7b9812bc09020be858e0ff4d561022caccc
Geanderson <geanderson@nexustreinamento.com>
2026-08-16 16:28:55 -0300

    seo: adicionar rota /marketing ao sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,6 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes = [
     '',
     '/exclusive',
+    '/marketing',
     '/dante-safra',
     '/excellence',
     '/about',
