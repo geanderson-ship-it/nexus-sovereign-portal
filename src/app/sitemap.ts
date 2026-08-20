0cb622a9b29c5a056fb085bc698f1c758cf69dc1
Geanderson <geanderson@nexustreinamento.com>
2026-08-20 06:28:54 -0300

    chore: update sitemap baseUrl to official nexusholdinggroup.com.br domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.nexustreinamento.com';
+  const baseUrl = 'https://www.nexusholdinggroup.com.br';
 
   // Static platform sections (excluding deleted courses, gallery, consulting, palestras)
   const staticRoutes = [
